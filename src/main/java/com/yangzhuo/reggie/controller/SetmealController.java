2db602ddeccb4d20c3814adb9163ce278f7a0d15
zhuo18329751762 <2199698862@qq.com>
2023-05-17 22:05:06 +0800

    使用SpringCache redis缓存优化

--- src/main/java/com/yangzhuo/reggie/controller/SetmealController.java
+++ src/main/java/com/yangzhuo/reggie/controller/SetmealController.java
@@ -13,6 +13,8 @@ import com.yangzhuo.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,6 +41,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息：{}",setmealDto);
         setmealService.saveWithDish(setmealDto);
@@ -93,6 +96,7 @@ public class SetmealController {
      */
     @DeleteMapping
     @Transactional
+    @CacheEvict(value = "setmealCache",allEntries = true)
     public R<String> delete(@RequestParam List<Long> ids){
         setmealService.removeWithDish(ids);
         return R.success("套餐数据删除成功");
@@ -100,6 +104,7 @@ public class SetmealController {
 
     //http://localhost:8080/setmeal/list?categoryId=1413342269393674242&status=1
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key = "#setmeal.categoryId+'_'+#setmeal.status")
     public R<List<Setmeal>> list( Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId()!=null,Setmeal::getCategoryId,setmeal.getCategoryId());
